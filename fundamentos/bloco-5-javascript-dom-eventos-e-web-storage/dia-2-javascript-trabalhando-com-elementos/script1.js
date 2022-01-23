b71c367643501c34d2cc802fbc628b91b7f014fc
Mariana Fernanda <ariana.fernanda@icloud.com>
2022-01-23 19:58:01 -0300

    Requisito 1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script1.js
@@ -0,0 +1,8 @@
+function requisito1() {
+    let texto1 = 'Exercício 5.2 - JavaScript DOM';
+    let tag1 = document.createElement('h1');
+    tag1.innerText = texto1;
+    document.body.appendChild(tag1);
+};
+requisito1();
+
